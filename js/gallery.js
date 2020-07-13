06ff9311de334a437d58a289084b17a6108c2a53
lewleww <lewlewonics@yahoo.com>
2020-07-12 22:45:34 -0700

    Version 1

---
+++ js/gallery.js
@@ -0,0 +1,56 @@
+const numOfImages = 35
+let imageArr = []
+
+function createImageArray() {
+    for (let i=0; i < numOfImages; i++) {
+        imageArr.push(i+1)
+    }
+}
+
+function shuffleArray(a) {
+    for (let i = a.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [a[i], a[j]] = [a[j], a[i]];
+    }
+    return a;
+}
+
+function populateGallery() {
+    createImageArray()
+    shuffleArray(imageArr)
+
+    let dom = ''
+    for (let i=0; i < numOfImages; i++) {
+        dom += `
+        <div class="gallery-image image-container">
+            <img src="images/nadeshikos/${imageArr[i]}.jpg" alt="image">
+        </div>
+        `
+    }
+    console.log(dom)
+    document.getElementById('images-container').innerHTML = dom;
+}
+
+populateGallery()
+
+const modal = document.getElementById('modal')
+const images = document.querySelectorAll('.gallery-image')
+
+images.forEach( image => {
+    image.addEventListener('click', () => {
+        let imagePath = image.querySelector('img').src
+        //Open Modal with correct details
+        modal.style.display = 'flex'
+        modal.classList.add('active')
+        
+        document.getElementById('chosen-image-link').href = imagePath
+        document.getElementById('chosen-image').src = imagePath
+    })
+})
+
+//Close modal if you click outside image
+window.addEventListener('click', (e) => {
+    if (e.target == modal) {
+        modal.style.display = 'none'
+    }
+})
